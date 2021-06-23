f0543ecf39697824d4fd674e150f0056870fe5d7
Ibne Nahian <evilprince2009@gmail.com>
2021-06-23 12:56:46 +0600

    home redirect and about route

--- EagleBlog/app.cpp
+++ EagleBlog/app.cpp
@@ -4,17 +4,25 @@
 
 int main()
 {
-	const int port_address = 5500;
+	const int port_address = 9100;
 	crow::SimpleApp app;
-	crow::mustache::set_base(".");
+	crow::mustache::set_base("./");
 	
-
 	CROW_ROUTE(app, "/")([]() {
-		crow::mustache::context view_context;
+		// crow::mustache::context view_context;
 		auto home_page = crow::mustache::load("index.html");
-		
 		return home_page.render();
-		});
+	});
+
+	CROW_ROUTE(app, "/blogs")([](const crow::request& req, crow::response& res) {
+		res.redirect("/");
+		res.end();
+	});
+
+	CROW_ROUTE(app, "/about")([]() {
+		auto about_page = crow::mustache::load("about.html");
+		return about_page.render();
+	});
 
 	app.port(port_address).multithreaded().run();
 	return 0;
